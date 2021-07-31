2e1986118763282f66b3b6e5e9dac953a81b0db3
huukhuong <huukhuong.it@gmail.com>
2021-07-31 08:41:49 +0700

    Update thêm phần loading

--- image_generate.cpp
+++ image_generate.cpp
@@ -3,7 +3,31 @@
 #include <string>
 #include <windows.h>
 #include <conio.h>
+
 using namespace std;
+
+void loading() {
+    system("color 0A");
+  
+    char a = 177, b = 219;
+  
+    cout << "\n\t\t\t\t\t\tLoading...\n\n";
+    cout << "\t\t\t\t\t";
+  
+    for (int i = 0; i < 26; i++)
+        cout << a;
+  
+    cout << "\r";
+    cout << "\t\t\t\t\t";
+  
+    for (int i = 0; i < 26; i++) {
+        cout << b;
+        Sleep(100);
+    }
+    cout << "\nDa load xong!!!";
+    Sleep(500);
+    system("cls");
+}
 
 int main()
 {
@@ -22,15 +46,16 @@ int main()
         newfile.close();
     }
 
-    cout << "Nhap enter de bat dau chay: ";
-    getch();
+    loading();
     cout << endl;
 
     for (int i = 0; i < str.length(); i++)
     {
         cout << str.at(i);
-//        Sleep(10);
+        if(str.at(i) == '\n')
+	        Sleep(100);
     }
-
+    
+	cin.ignore();
     return 0;
 }
